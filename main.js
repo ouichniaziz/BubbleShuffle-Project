edcc37783a1af8eaec63eebe20394d371440ec6f
ouichniaziz <medazizouichni@gmail.com>
2021-04-15 09:50:11 +0100

    base

---
+++ main.js
@@ -0,0 +1,17 @@
+var passwordOutput = document.getElementById('password-output')
+var arrayLowerCase = "abcdefghhijklmnopqrstuvwxyz".split('')
+var arrayUpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('')
+var arrayNumbers = "0123456789".split('')
+var arraySymbols = '!$@#-=^<>%£°&~'.split('')
+
+
+function generatePassword () {
+    
+    var array = []
+    if (lowercase.checked) {
+        
+    }
+        
+    
+
+}
